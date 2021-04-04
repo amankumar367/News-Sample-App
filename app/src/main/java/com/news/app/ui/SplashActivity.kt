48c742d13a09766a6500c38eb1c6580cdbdd0e9d
AMAN KUMAR <kumaraman367@gmail.com>
2021-04-04 22:59:05 +0530

    Added App icon & splash icon in SplashActivity

--- app/src/main/java/com/news/app/ui/SplashActivity.kt
+++ app/src/main/java/com/news/app/ui/SplashActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.AnimationUtils
 import com.news.app.R
 import com.news.app.ui.articles.ArticleActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in_center)
+        ivSplash.startAnimation(animationFadeIn)
 
         Handler(Looper.getMainLooper()).postDelayed({
             ArticleActivity.start(this)
