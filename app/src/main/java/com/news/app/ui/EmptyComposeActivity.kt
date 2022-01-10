3e19ebb6a16e6cd25ab19518d8fbf5d33f2724c1
Aman Kumar <kumaraman367@gmail.com>
2022-01-10 08:58:45 +0530

    Compose Learing:  - adding compose in existing App  - Compose UI tweaking  - row/colomn

--- app/src/main/java/com/news/app/ui/EmptyComposeActivity.kt
+++ app/src/main/java/com/news/app/ui/EmptyComposeActivity.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -53,13 +55,17 @@ private fun Greeting(name: String) {
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
         shape = RoundedCornerShape(15.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp),
-        ) {
-            Text(text = "Hello, ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1F)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+
+            OutlinedButton(
+                onClick = { /* TODO */ }
+            ) {
+                Text("Show more")
+            }
         }
     }
 }
