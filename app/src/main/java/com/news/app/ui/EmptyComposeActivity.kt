4de338187a1e15fa7b432a7a4ac73094e5171c4d
Aman Kumar <kumaraman367@gmail.com>
2022-01-17 00:47:58 +0530

    (learing) - state hosting

--- app/src/main/java/com/news/app/ui/EmptyComposeActivity.kt
+++ app/src/main/java/com/news/app/ui/EmptyComposeActivity.kt
@@ -7,21 +7,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.news.app.ui.theme.NewsSampleAppTheme
 
 class EmptyComposeActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,13 +43,22 @@ class EmptyComposeActivity : ComponentActivity() {
             context.startActivity(Intent(context, EmptyComposeActivity::class.java))
         }
     }
+
 }
 
 @Composable
-fun MyApp(
-    names: List<String> = listOf(
-        "World", "Compose", "Aman", "Vaibhav", "Jayesh", "JK"
-    )
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(
+    names: List<String> = listOf("World", "Compose", "Aman", "Vaibhav", "Jayesh", "JK")
 ) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         names.forEach { Greeting(name = it) }
@@ -78,10 +94,37 @@ private fun Greeting(name: String) {
     }
 }
 
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinueClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NewsSampleAppTheme {
-        MyApp()
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    NewsSampleAppTheme {
+        OnboardingScreen(onContinueClicked = { })
     }
 }
