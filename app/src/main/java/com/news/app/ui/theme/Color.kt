1ecca8f423ca65a0ecffc44942070657f6a5f472
Aman Kumar <kumaraman367@gmail.com>
2022-02-08 21:04:51 +0530

    migrated splash screen from xml to compose

--- app/src/main/java/com/news/app/ui/theme/Color.kt
+++ app/src/main/java/com/news/app/ui/theme/Color.kt
@@ -2,7 +2,24 @@ package com.news.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val navyBlue500 = Color(0xFF14192C)
+val navyBlue700 = Color(0xFF0A0F22)
+
+val green200 = Color(0xffa5d6a7)
+val green500 = Color(0xff4caf50)
+val green700 = Color(0xff388e3c)
+
+val blue200 = Color(0xFF9FA8DA)
+val blue500 = Color(0xFF3F51B5)
+val blue700 = Color(0xFF303F9F)
+
+val purple200 = Color(0xFFB39DDB)
+val purple500 = Color(0xFF6200EE)
+val purple700 = Color(0xFF3700B3)
+
+val orange200 = Color(0xFFff7961)
+val orange500 = Color(0xFFf44336)
+val orange700 = Color(0xFFba000d)
+
+val gray = Color(0xFF464646)
+val lightGray = Color(0xFFA4A8AD)
