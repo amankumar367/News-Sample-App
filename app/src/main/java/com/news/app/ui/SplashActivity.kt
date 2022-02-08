8878443213c4d0f789fce88e551a0c93756d8e43
Aman Kumar <kumaraman367@gmail.com>
2022-02-08 21:04:51 +0530

    migrated splash screen from xml to compose

--- app/src/main/java/com/news/app/ui/SplashActivity.kt
+++ app/src/main/java/com/news/app/ui/SplashActivity.kt
@@ -1,26 +1,64 @@
 package com.news.app.ui
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.animation.AnimationUtils
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.news.app.R
 import com.news.app.ui.articles.ArticleActivity
-import kotlinx.android.synthetic.main.activity_splash.*
+import com.news.app.ui.theme.NewsSampleAppTheme
+import com.news.app.ui.theme.gray
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContent {
+            NewsSampleAppTheme {
+                SplashScreen()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    ArticleActivity.launch(this)
+                }, 2000)
+            }
+        }
+    }
 
-        val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in_center)
-        ivSplash.startAnimation(animationFadeIn)
+    @Composable
+    fun SplashScreen() {
+        Surface {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(gray),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_news),
+                    contentDescription = "",
+                )
+            }
+        }
+    }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            ArticleActivity.launch(this)
-        }, 2000)
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        NewsSampleAppTheme {
+            SplashScreen()
+        }
     }
 }
--- app/src/main/java/com/news/app/ui/theme/Theme.kt
+++ app/src/main/java/com/news/app/ui/theme/Theme.kt
@@ -5,17 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = navyBlue500,
+    primaryVariant = navyBlue700,
+    secondary = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = navyBlue500,
+    primaryVariant = navyBlue700,
+    secondary = Color.White
 )
 
 @Composable
