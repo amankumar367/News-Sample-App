30e5167f8a1cb68b424c0334b3bf55cbfbaa9597
Aman Kumar <kumaraman367@gmail.com>
2022-02-12 03:03:08 +0530

    migrated article details screen from xml to compose [100%]

--- app/src/main/java/com/news/app/ui/articles/ArticleDetailsActivity.kt
+++ app/src/main/java/com/news/app/ui/articles/ArticleDetailsActivity.kt
@@ -4,22 +4,22 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -31,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.news.app.R
 import com.news.app.data.models.Article
+import com.news.app.extensions.formatDate
 import com.news.app.ui.theme.NewsSampleAppTheme
 import com.news.app.ui.theme.transparentBlack
 import dagger.hilt.android.AndroidEntryPoint
@@ -52,9 +53,12 @@ class ArticleDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        readParams()
+
         setContent {
             NewsSampleAppTheme {
-                readParams()
                 ArticleDetailsScreen()
             }
         }
@@ -68,43 +72,114 @@ class ArticleDetailsActivity : AppCompatActivity() {
     @Composable
     private fun ArticleDetailsScreen() {
         Surface {
-            Column(modifier = Modifier.fillMaxSize()) {
-                var sizeImage by remember { mutableStateOf(IntSize.Zero) }
-                val gradient = Brush.verticalGradient(
-                    colors = listOf(Color.Transparent, Color.Black),
-                    startY = sizeImage.height.toFloat() / 3,
-                    endY = sizeImage.height.toFloat()
+            val scrollState = rememberScrollState(0)
+            var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+            val gradient = Brush.verticalGradient(
+                colors = listOf(Color.Transparent, Color.Black),
+                startY = sizeImage.height.toFloat() / 3,
+                endY = sizeImage.height.toFloat()
+            )
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = rememberImagePainter(getImageUrl(article.urlToImage, article.url)),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .onGloballyPositioned { sizeImage = it.size }
+                        .fillMaxSize()
                 )
 
-                Box {
-                    Image(
-                        painter = rememberImagePainter(getImageUrl(article.urlToImage, article.url)),
-                        contentDescription = "",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .onGloballyPositioned { sizeImage = it.size }
-                            .fillMaxSize()
+                Box(modifier = Modifier
+                    .matchParentSize()
+                    .background(gradient))
+
+                IconButton(
+                    onClick = { onBackPressed() },
+                    modifier = Modifier
+                        .padding(horizontal = 6.dp, vertical = 30.dp)
+                        .align(Alignment.TopStart)
+                        .background(transparentBlack, CircleShape)
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.White
                     )
+                }
 
-                    Box(modifier = Modifier.matchParentSize().background(gradient))
-
-                    IconButton(
-                        onClick = { onBackPressed() },
-                        modifier = Modifier
-                            .padding(4.dp)
-                            .align(Alignment.TopStart)
-                            .background(transparentBlack, CircleShape)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.ArrowBack,
-                            contentDescription = null,
-                            tint = Color.White
-                        )
-                    }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(320.dp)
+                        .align(BottomCenter)
+                ) {
+                    ScrollableContent(scrollState)
                 }
             }
         }
     }
+
+    @Composable
+    private fun ScrollableContent(scrollState: ScrollState) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 20.dp, vertical = 0.dp)
+                .verticalScroll(state = scrollState)
+        ) {
+            ArticleTitle(article.title.orEmpty())
+            Spacer(modifier = Modifier.height(30.dp))
+            ArticleAuthorAndPublishTime(article.author.orEmpty(), article.publishedAt.orEmpty())
+            Spacer(modifier = Modifier.height(10.dp))
+            ArticleDescription(article.description.orEmpty())
+            Spacer(modifier = Modifier.height(30.dp))
+        }
+    }
+
+    @Composable
+    fun ArticleTitle(title: String, modifier: Modifier = Modifier) {
+        Text(
+            text = title,
+            color = Color.White,
+            style = typography.h5,
+            modifier = modifier,
+            maxLines = 3
+        )
+    }
+
+    @Composable
+    private fun ArticleDescription(description: String, modifier: Modifier = Modifier) {
+        Text(
+            text = description,
+            color = Color.White,
+            style = typography.body2,
+            modifier = modifier
+        )
+    }
+
+    @Composable
+    private fun ArticleAuthorAndPublishTime(
+        author: String,
+        publishedAt: String,
+        modifier: Modifier = Modifier
+    ) {
+        Row(modifier = modifier.fillMaxWidth()) {
+            Text(
+                text = author,
+                color = Color.White,
+                style = typography.body1,
+                modifier = Modifier.weight(3F),
+                maxLines = 1
+            )
+            Text(
+                text = publishedAt.formatDate(),
+                color = Color.White,
+                style = typography.body1,
+                modifier = Modifier.weight(1F),
+            )
+        }
+    }
+
 
     private fun getImageUrl(url: String?, articleUrl: String?): String {
         return if (url == null) {
--- app/src/main/java/com/news/app/ui/theme/Type.kt
+++ app/src/main/java/com/news/app/ui/theme/Type.kt
@@ -2,15 +2,40 @@ package com.news.app.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.news.app.R
+
+
+// FontFamily
+val fonts = FontFamily(
+    Font(R.font.roboto_slab_regular),
+    Font(R.font.roboto_slab_bold, weight = FontWeight.Bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    h5 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
 )
