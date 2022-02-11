4751fa50cbeb132049f01d9c4ef0d4212a89fe2e
Aman Kumar <kumaraman367@gmail.com>
2022-02-11 22:03:13 +0530

    partially migrated article details screen from xml to compose

--- app/src/main/java/com/news/app/ui/articles/ArticleDetailsActivity.kt
+++ app/src/main/java/com/news/app/ui/articles/ArticleDetailsActivity.kt
@@ -2,12 +2,37 @@ package com.news.app.ui.articles
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.news.app.R
 import com.news.app.data.models.Article
-import com.news.app.databinding.ActivityArticleDetailsBinding
+import com.news.app.ui.theme.NewsSampleAppTheme
+import com.news.app.ui.theme.transparentBlack
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,18 +48,77 @@ class ArticleDetailsActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var viewBinding: ActivityArticleDetailsBinding
+    private lateinit var article: Article
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_article_details)
+        setContent {
+            NewsSampleAppTheme {
+                readParams()
+                ArticleDetailsScreen()
+            }
+        }
+    }
 
-        val article: Article = intent.getParcelableExtra(BUNDLE_ARTICLE)
+    private fun readParams() {
+        article = intent.getParcelableExtra(BUNDLE_ARTICLE)
             ?: throw Error("Article details are required to launch")
+    }
 
-        viewBinding.data = article
+    @Composable
+    private fun ArticleDetailsScreen() {
+        Surface {
+            Column(modifier = Modifier.fillMaxSize()) {
+                var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+                val gradient = Brush.verticalGradient(
+                    colors = listOf(Color.Transparent, Color.Black),
+                    startY = sizeImage.height.toFloat() / 3,
+                    endY = sizeImage.height.toFloat()
+                )
 
-        viewBinding.ivBack.setOnClickListener { onBackPressed() }
+                Box {
+                    Image(
+                        painter = rememberImagePainter(getImageUrl(article.urlToImage, article.url)),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .onGloballyPositioned { sizeImage = it.size }
+                            .fillMaxSize()
+                    )
+
+                    Box(modifier = Modifier.matchParentSize().background(gradient))
+
+                    IconButton(
+                        onClick = { onBackPressed() },
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .align(Alignment.TopStart)
+                            .background(transparentBlack, CircleShape)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.ArrowBack,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getImageUrl(url: String?, articleUrl: String?): String {
+        return if (url == null) {
+            val iconUrl = "https://besticon-demo.herokuapp.com/icon?url=%s&size=80..120..200"
+            String.format(iconUrl, Uri.parse(articleUrl).authority)
+        } else url
+    }
+
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        NewsSampleAppTheme {
+            ArticleDetailsScreen()
+        }
     }
 
 }
